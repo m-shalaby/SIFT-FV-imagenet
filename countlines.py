31376256c5452ff50cf7cfbfdd62c057b763061a
Mohammed Shalaby <mohammed.shalaby@mail.mcgill.ca>
2018-12-30 16:39:15 -0500

    Update countlines.py

--- countlines.py
+++ countlines.py
@@ -1,3 +1,5 @@
+#the purpose of this is to count the number of lines in the massive visual bag of words csv files to allow the creation of memory mapped files
+
 import csv
 
 with open('/Users/m_ayman/Desktop/Life/Courses/Fall 2018/Comp551/Final Project/ALLDATAcomp/vbow_train.csv', 'r') as f:
